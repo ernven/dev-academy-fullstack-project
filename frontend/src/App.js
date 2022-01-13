941570cd1b878d91473219028051658df0e5012f
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2022-01-13 13:16:00 +0200

    Improved table grid.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import { Typography } from '@mui/material'
 import useFetch from './utils/useFetch'
 import HeaderDrawer from './components/HeaderDrawer/HeaderDrawer'
 import DataGrid from './components/DataTable/DataGrid'
-
 import Graph from './components/Graphs/Chart'
 import AdminPage from './components/AdminPage/AdminPage'
 import Dashboard from './components/Dashboard/Dashboard'
--- frontend/src/components/DataTable/DataGrid.js
+++ frontend/src/components/DataTable/DataGrid.js
@@ -1,51 +1,51 @@
-import {AgGridColumn, AgGridReact} from 'ag-grid-react'
+import { useState, useMemo } from 'react'
+import { AgGridReact } from '@ag-grid-community/react'
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model'
 
 import useFetch from '../../utils/useFetch'
 
-import 'ag-grid-community/dist/styles/ag-grid.css'
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
+import '@ag-grid-community/core/dist/styles/ag-grid.css'
+import '@ag-grid-community/core/dist/styles/ag-theme-material.css'
 
 export default function DataGrid() {
+
   const { data } = useFetch('data')
 
-  // Date picker??
+  // Defining the data and columns, memoized to prevent unnecessary updates.
+  const farmsData = useMemo(() => data ? data : [], [data])
+
+  const columnDefs = useMemo(() => [
+    {
+      headerName: 'Farm Name', field: 'farm_name', filter: true, floatingFilter: true, flex: 1.5
+    },
+    {
+      headerName: 'Date', field: 'date', valueFormatter: row => (new Date(row.value)).toLocaleString(), flex: 1.5
+    },
+    {
+      headerName: 'Type', field: 'entry_type', valueFormatter: row => row.value === 'rainFall' ? 'rainfall' : row.value,
+      filter: true, floatingFilter: true, flex: 1
+    },
+    {
+      headerName: 'Value', field: 'read_value', filter: true, floatingFilter: true, flex: 1
+    },
+  ], [])
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+  }), [])
+
+  const modules = useMemo(() => [ClientSideRowModelModule], [])
 
   return (
-      <div className='ag-theme-alpine' style={{height: '80vh', width: '60vw'}}>
-          <AgGridReact rowData={data}>
-              <AgGridColumn
-                headerName='Farm Name'
-                field='farm_name'
-                sortable={true}
-                filter={true}
-                floatingFilter={true}
-                flex={1.5}
-              />
-              <AgGridColumn
-                headerName='Date'
-                field='date'
-                valueFormatter={row => (new Date(row.value)).toLocaleString()}
-                sortable={true}
-                flex={1.5}
-              />
-              <AgGridColumn
-                headerName='Type'
-                field='entry_type'
-                valueFormatter={row => row.value === 'rainFall' ? 'rainfall' : row.value}
-                sortable={true}
-                filter={true}
-                floatingFilter={true}
-                flex={1}
-              />
-              <AgGridColumn
-                headerName='Value'
-                field='read_value'
-                sortable={true}
-                filter={true}
-                floatingFilter={true}
-                flex={1}
-              />
-          </AgGridReact>
-      </div>
+  <div style={{height: '80vh', width: '60vw'}}>
+    <AgGridReact
+      className='ag-theme-material'
+      animateRows='true'
+      modules={modules}
+      columnDefs={columnDefs}
+      defaultColDef={defaultColDef}
+      rowData={farmsData}>
+    </AgGridReact>
+  </div>
   )
 }
