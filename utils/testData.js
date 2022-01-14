a01679a1c9f86c4317c8c513d8f35cce5325d2c7
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2022-01-14 15:49:17 +0200

    Added more sample data.

--- utils/testData.js
+++ utils/testData.js
@@ -1,20 +1,278 @@
 export const farms = [
   {
-    "id": "b33aa9e9-1538-11ec-9a23-000000000000",
-    "farm_name": "Friman Metsola collective"
+    id: "b33aa9e9-1538-11ec-9a23-000000000000",
+    farm_name: "Friman Metsola collective"
   },
   {
-    "id": "b45230e6-1538-11ec-9a23-000000000000",
-    "farm_name": "Noora's farm"
+    id: "b45230e6-1538-11ec-9a23-000000000000",
+    farm_name: "Noora's farm"
   },
   {
-    "id": "c5d42f90-1538-11ec-9a23-000000000000",
-    "farm_name": "Organic Ossi's Impact That Lasts plantase"
+    id: "c5d42f90-1538-11ec-9a23-000000000000",
+    farm_name: "Organic Ossi's Impact That Lasts plantase"
   },
   {
-    "id": "dc5874c0-1538-11ec-9a23-000000000000",
-    "farm_name": "PartialTech Research Farm"
+    id: "dc5874c0-1538-11ec-9a23-000000000000",
+    farm_name: "PartialTech Research Farm"
   }
 ]
 
-export const 
+export const averages = [
+  {
+    farm_name: "Noora's farm",
+    entry_type: "temperature",
+    average_value: "10.53"
+  },
+  {
+    farm_name: "Noora's farm",
+    entry_type: "pH",
+    average_value: "6.13"
+  },
+  {
+    farm_name: "Friman Metsola collective",
+    entry_type: "temperature",
+    average_value: "15.47"
+  },
+  {
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    entry_type: "pH",
+    average_value: "6.74"
+  },
+  {
+    farm_name: "Noora's farm",
+    entry_type: "rainFall",
+    average_value: "3.58"
+  },
+  {
+    farm_name: "PartialTech Research Farm",
+    entry_type: "temperature",
+    average_value: "8.68"
+  },
+  {
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    entry_type: "temperature",
+    average_value: "5.60"
+  },
+  {
+    farm_name: "Friman Metsola collective",
+    entry_type: "pH",
+    average_value: "6.20"
+  },
+  {
+    farm_name: "Friman Metsola collective",
+    entry_type: "rainFall",
+    average_value: "2.22"
+  },
+  {
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    entry_type: "rainFall",
+    average_value: "3.24"
+  },
+  {
+    farm_name: "PartialTech Research Farm",
+    entry_type: "rainFall",
+    average_value: "3.10"
+  },
+  {
+    farm_name: "PartialTech Research Farm",
+    entry_type: "pH",
+    average_value: "5.78"
+  }
+]
+
+export const extremeValues = [
+  {
+    farm_name: "Noora's farm",
+    entry_type: "temperature",
+    max_value: "33.20",
+    min_value: "-22.20"
+  },
+  {
+    farm_name: "Noora's farm",
+    entry_type: "pH",
+    max_value: "6.63",
+    min_value: "5.63"
+  },
+  {
+    farm_name: "Friman Metsola collective",
+    entry_type: "temperature",
+    max_value: "37.70",
+    min_value: "-18.10"
+  },
+  {
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    entry_type: "pH",
+    max_value: "7.23",
+    min_value: "6.24"
+  },
+  {
+    farm_name: "Noora's farm",
+    entry_type: "rainFall",
+    max_value: "195.80",
+    min_value: "0.00"
+  },
+  {
+    farm_name: "PartialTech Research Farm",
+    entry_type: "temperature",
+    max_value: "30.50",
+    min_value: "-24.80"
+  },
+  {
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    entry_type: "temperature",
+    max_value: "27.50",
+    min_value: "-28.60"
+  },
+  {
+    farm_name: "Friman Metsola collective",
+    entry_type: "pH",
+    max_value: "6.70",
+    min_value: "5.70"
+  },
+  {
+    farm_name: "Friman Metsola collective",
+    entry_type: "rainFall",
+    max_value: "174.50",
+    min_value: "0.00"
+  },
+  {
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    entry_type: "rainFall",
+    max_value: "195.90",
+    min_value: "0.00"
+  },
+  {
+    farm_name: "PartialTech Research Farm",
+    entry_type: "rainFall",
+    max_value: "192.70",
+    min_value: "0.00"
+  },
+  {
+    farm_name: "PartialTech Research Farm",
+    entry_type: "pH",
+    max_value: "6.49",
+    min_value: "5.09"
+  }
+]
+
+export const data = [
+  {
+    entry_id: "1",
+    farm_name: "Friman Metsola collective",
+    date: "2019-01-01T04:09:47.373Z",
+    entry_type: "temperature",
+    read_value: "-9.00"
+  },
+  {
+    entry_id: "2",
+    farm_name: "Friman Metsola collective",
+    date: "2019-01-01T19:04:09.813Z",
+    entry_type: "temperature",
+    read_value: "-12.20"
+  },
+  {
+    entry_id: "3",
+    farm_name: "Friman Metsola collective",
+    date: "2019-01-01T09:49:37.397Z",
+    entry_type: "temperature",
+    read_value: "-8.90"
+  },
+  {
+    entry_id: "13558",
+    farm_name: "Noora's farm",
+    date: "2021-03-20T16:37:05.339Z",
+    entry_type: "temperature",
+    read_value: "11.90"
+  },
+  {
+    entry_id: "13561",
+    farm_name: "Noora's farm",
+    date: "2021-03-21T08:28:55.695Z",
+    entry_type: "temperature",
+    read_value: "14.30"
+  },
+  {
+    entry_id: "13562",
+    farm_name: "Noora's farm",
+    date: "2021-03-21T05:39:50.290Z",
+    entry_type: "temperature",
+    read_value: "14.30"
+  },
+  {
+    entry_id: "22287",
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    date: "2021-08-12T14:18:13.457Z",
+    entry_type: "temperature",
+    read_value: "21.50"
+  },
+  {
+    entry_id: "30570",
+    farm_name: "PartialTech Research Farm",
+    date: "2021-11-28T21:55:32.236Z",
+    entry_type: "temperature",
+    read_value: "-4.80"
+  },
+  {
+    entry_id: "7",
+    farm_name: "Friman Metsola collective",
+    date: "2019-01-01T21:10:20.551Z",
+    entry_type: "pH",
+    read_value: "6.28"
+  },
+  {
+    entry_id: "11545",
+    farm_name: "Noora's farm",
+    date: "2020-06-05T06:04:04.081Z",
+    entry_type: "pH",
+    read_value: "6.43"
+  },
+  {
+    entry_id: "23085",
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    date: "2021-11-28T22:11:49.013Z",
+    entry_type: "pH",
+    read_value: "6.93"
+  },
+  {
+    entry_id: "23104",
+    farm_name: "PartialTech Research Farm",
+    date: "2019-01-02T03:38:33.564Z",
+    entry_type: "pH",
+    read_value: "6.21"
+  },
+  {
+    entry_id: "8",
+    farm_name: "Friman Metsola collective",
+    date: "2019-01-01T21:10:20.551Z",
+    entry_type: "rainFall",
+    read_value: "0.00"
+  },
+  {
+    entry_id: "13773",
+    farm_name: "Noora's farm",
+    date: "2021-04-20T17:26:07.115Z",
+    entry_type: "rainFall",
+    read_value: "0.00"
+},
+{
+    entry_id: "13783",
+    farm_name: "Noora's farm",
+    date: "2021-04-21T00:52:27.684Z",
+    entry_type: "rainFall",
+    read_value: "0.00"
+  },
+  {
+    entry_id: "20190",
+    farm_name: "Organic Ossi's Impact That Lasts plantase",
+    date: "2020-11-01T10:13:40.759Z",
+    entry_type: "rainFall",
+    read_value: "3.50"
+  },
+  {
+    entry_id: "30563",
+    farm_name: "PartialTech Research Farm",
+    date: "2021-11-29T00:46:19.715Z",
+    entry_type: "rainFall",
+    read_value: "1.70"
+  }
+]
