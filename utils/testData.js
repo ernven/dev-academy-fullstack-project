c05e58e6f7cd086a67335b42317d08a0126c734c
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2022-01-14 13:23:04 +0200

    Added base for testing.

---
+++ utils/testData.js
@@ -0,0 +1,20 @@
+export const farms = [
+  {
+    "id": "b33aa9e9-1538-11ec-9a23-000000000000",
+    "farm_name": "Friman Metsola collective"
+  },
+  {
+    "id": "b45230e6-1538-11ec-9a23-000000000000",
+    "farm_name": "Noora's farm"
+  },
+  {
+    "id": "c5d42f90-1538-11ec-9a23-000000000000",
+    "farm_name": "Organic Ossi's Impact That Lasts plantase"
+  },
+  {
+    "id": "dc5874c0-1538-11ec-9a23-000000000000",
+    "farm_name": "PartialTech Research Farm"
+  }
+]
+
+export const 
