aff2c25081f02892fb40453dc280d1e8f19239b8
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2022-01-15 09:11:02 +0200

    Changed conn string env var.

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,8 @@ export const appConfig = {
 
 export const dbConfig = {
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
+  // We can pass an URL String instead of all parameters separately.
+  connection: process.env.DATABASE_URL,
   /*
   connection: {
     host: process.env.DB_URI,
